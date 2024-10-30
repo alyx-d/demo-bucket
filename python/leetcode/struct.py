3bc67cb3f8b75ca90afbf3662765e1df91aae677
qt <sanaki@qq.com>
2024-10-31 03:42:06 +0800

    struct

---
+++ python/leetcode/struct.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    @staticmethod
+    def build(arr: List[int]):
+        v = ListNode()
+        curr = v
+        for it in arr:
+            curr.next = ListNode(it)
+            curr = curr.next
+        return v.next
+
+    def __str__(self):
+        curr = self
+        result = "["
+        seq = ""
+        while curr:
+            result += seq + str(curr.val)
+            seq = ","
+            curr = curr.next
+        return result + "]"
