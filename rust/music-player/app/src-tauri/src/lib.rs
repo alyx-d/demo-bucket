45f6f1dc910eeb20d61e24ba548158a5ffdab471
qt <sanaki@qq.com>
2024-11-12 21:42:30 +0800

    log

--- rust/music-player/app/src-tauri/src/lib.rs
+++ rust/music-player/app/src-tauri/src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use std::sync::Mutex;
+use std::{path::PathBuf, sync::Mutex};
 
 use tauri::Manager;
 
@@ -15,7 +15,18 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_log::Builder::new().build())
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .level(log::LevelFilter::Info)
+                .target(tauri_plugin_log::Target::new(
+                    tauri_plugin_log::TargetKind::Folder {
+                        path: PathBuf::from("./logs"),
+                        file_name: Some("logs".to_string()),
+                    },
+                ))
+                .rotation_strategy(tauri_plugin_log::RotationStrategy::KeepAll)
+                .build(),
+        )
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
